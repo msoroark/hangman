57bf55746d381e9e65d112dfa619496d87606702
Meg O'Roark <msoroark@gmail.com>
2017-07-14 16:36:32 -0500

    js completed commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,52 @@
+var wordCombos = [{
+	word: 'peppermint butler', 
+	phrase: 'Loyal and faithful butler of Princess Bubble Gum. Also possibly some sort of demon. Who can say?',
+	videoID: 'NxQOD7DmxmE'
+}, {
+	word: 'bacon pancakes',
+	phrase: 'Take some bacon and put it in a pancake!',
+	videoID: 'FjxAAezqJZQ',
+}, {
+	word: 'treetrunks',
+	phrase: 'A sassy yellow elephant from Candy Kingdom, she has no time for messing around.',
+	videoID: '9blaYtgZk-E',
+}, {
+	word: 'gunter',
+	phrase: 'Gunters real name is Orgalorg. While is main goal is domination of the solar sytem, he mostly just hangs out with his best friend, the Ice King',
+	videoID: 'QscSV4lxw7Q',
+}, {
+	word: 'lumpy space',
+	phrase: 'Home of Lumpy space princess. Oh my glob!',
+	videoID: 'AAmJslV48g',
+}, {
+	word: 'lady rainicorn',
+	phrase: 'Lady rainicorn is the Korean speaking girlfriend of Jake. Also, like, half rainbow half unicorn.',
+	videoID: 'LErwHmjBuYQ#t=0m35s',
+}, ];
 
+function getWord(){
+	return wordCombos.shift();
+}
 
-var words = ['Peppermint Butler', 'Bacon Pancakes', 'Algebraic', 'Jiggler', 'Candy Kingdom', 'Mathmatical', 'Lumpy Space', 'Turtle Princess'];
-var correctAnswer = words[Math.floor(Math.random() * words.length)];
-	console.log(correctAnswer);
+var wordCombo = getWord();
+var correctAnswer = wordCombo.word;
+var winningPhrase = wordCombo.phrase;
 var wins = 0;
 var guesses = 15;
 
-function newWord() {
-	//get new word
-	correctAnswer = words[Math.floor(Math.random() * words.length)];
-	console.log(correctAnswer);
+
+
+
+function newGame() {
+	//get new word]
+	// correctAnswer = words[Math.floor(Math.random() * words.length)];
+	wordCombo = getWord();
+	console.log(wordCombo);
+	correctAnswer = wordCombo.word;
+	winningPhrase = wordCombo.phrase;
+	currentWord = makeUnderscores(correctAnswer);
+
+	document.querySelector('#current_word').innerHTML = currentWord;
 	//clear guesses so far
 	document.querySelector('#g_sofar').innerHTML = "";
 	//reset guesses 
@@ -17,6 +54,26 @@ function newWord() {
 
 }
 
+ document.getElementById("button").onclick = function(){
+ 	newGame()};
+ 	
+
+
+//Display for when game is won or lost
+function finishGame() {
+	if (currentWord === correctAnswer){
+	//fishish game text
+	document.querySelector('#finish_game_text').innerHTML = 'Mathmatical! You got it!';
+	document.querySelector('#phrase').innerHTML = wordCombo.phrase;
+	var videoHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' +
+		wordCombo.videoID +  '?&autoplay=1"frameborder="0" allowfullscreen></iframe>'
+	document.querySelector('#video').innerHTML = videoHTML;
+	//phrase displayed
+	} else {
+	document.querySelector('#finish_game_text').innerHTML = 'UNACCEPTABLE! Get it together, dude.';
+	document.querySelector('#video').innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/07So_lJQyqw" frameborder="0" allowfullscreen></iframe>'
+	}
+}	
 // I want to display current word-now I want it to be underscored. 
 
 function makeUnderscores(correctAnswer) {
@@ -60,6 +117,7 @@ function replaceIndices(stringToReplace, positions, replaceWith) {
 
 
 var currentWord = makeUnderscores(correctAnswer);
+console.log(currentWord)
 document.querySelector("#current_word").innerHTML = currentWord;
 
 
@@ -75,10 +133,16 @@ document.onkeyup = function(event) {
 	if (indices.length === 0) { //user guesses wrong
 		guesses -= 1;
 		if (guesses === 0){
-			newWord()
+			finishGame()
 		}
 	} else {
 		currentWord = replaceIndices(currentWord, indices, userGuess);
+	}
+
+
+	if (currentWord === correctAnswer) {
+		wins +=1;
+		finishGame()
 	}
 
 
